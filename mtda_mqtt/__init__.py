fd0c68d0ab88fba07c466bf6c6395a3c7d10fef4
anushayadav01 <Anusha_Yadav@mentor.com>
2022-09-14 12:55:33 +0530

    changes made

--- mtda_mqtt/__init__.py
+++ mtda_mqtt/__init__.py
@@ -9,10 +9,8 @@
 # SPDX-License-Identifier: MIT
 # ---------------------------------------------------------------------------
 
-from .__version__ import __version__
 
 __license__ = 'MIT'
 __copyright__ = 'Copyright (C) 2021 Siemens Digital Industries Software'
 
-__all__ = ['__version__']
 
