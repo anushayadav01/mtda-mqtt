a67f04db8421047fb8f545d14901237b4e601572
anushayadav01 <Anusha_Yadav@mentor.com>
2022-09-21 19:38:45 +0530

    changes are committed

--- mtda_mqtt/main.py
+++ mtda_mqtt/main.py
@@ -1,12 +1,20 @@
 import os
 import sys
+import socket
+import configparser
 
 
 class MTDAMqtt(object):
+
     def __init__(self):
-        self.name="Anusha"
+        self.name = socket.gethostname()
+        self.ctrlport = 5556
+        self.conport = 5557
+        self.debug_level = 0
+        self.is_remote = False
+        self.is_server = False
+        self.remote = None
+        self.socket = None
 
-
-if __name__=="__main__":
-    obj1=MTDAMqtt()
-    print(obj1.name)
+    def debug(self, lvl, msg):
+        if self.debug
